fe26bf909054f61d5ce3fb44d1401e1cb59084f0
Iannnn <232410103085@mail.unej.ac.id>
2024-06-23 23:09:22 +0700

    finishing UI

--- script.js
+++ script.js
@@ -1,18 +1,18 @@
-// event pada saat link navigation di klik
-$(".nav-item").on("click", function (e) {
-  // ambil isi href
-  var tujuan = $(this).attr("href");
-  // tangkap elemen yang bersangkutan
-  var elemenTujuan = $(tujuan);
+// // event pada saat link navigation di klik
+// $(".nav-item").on("click", function (e) {
+//   // ambil isi href
+//   var tujuan = $(this).attr("href");
+//   // tangkap elemen yang bersangkutan
+//   var elemenTujuan = $(tujuan);
 
-  // pindahkan scroll
-  $("body").animate(
-    {
-      scrollTop: elemenTujuan.offset().top - 50,
-    },
-    1250,
-    "easeInOutExpo"
-  );
+//   // pindahkan scroll
+//   $("body").animate(
+//     {
+//       scrollTop: elemenTujuan.offset().top - 50,
+//     },
+//     1250,
+//     "easeInOutExpo"
+//   );
 
-  e.preventDefault();
-});
+//   e.preventDefault();
+// });
