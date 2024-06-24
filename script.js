ba28c0d83fd2fdc976984cc1af24a20de452afcd
Iannnn <232410103085@mail.unej.ac.id>
2024-06-24 14:51:08 +0700

    Update Send Form

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-// const scriptURL = "https://script.google.com/macros/s/AKfycbz5yiohaRJrgCVGMcOrlbLy4h57v7U6AQsiGFNFCHU5OPytWRDsIHGzQCTmVrCr_cQ/exec";
-// const form = document.forms["Kotak Masuk"];
+let scriptURL = "https://script.google.com/macros/s/AKfycbz5yiohaRJrgCVGMcOrlbLy4h57v7U6AQsiGFNFCHU5OPytWRDsIHGzQCTmVrCr_cQ/exec";
+let form = document.forms["Kotak Masuk"];
 
-// form.addEventListener("submit", (e) => {
-//   e.preventDefault();
-//   fetch(scriptURL, { method: "POST", body: new FormData(form) })
-//     .then((response) => console.log("Success!", response))
-//     .catch((error) => console.error("Error!", error.message));
-// });
+let btnKirim = document.querySelector(".btn-kirim");
+let btnLoading = document.querySelector(".btn-loading");
+let Alert = document.querySelector(".alert");
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  btnKirim.classList.toggle("d-none");
+  btnLoading.classList.toggle("d-none");
+
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => {
+      btnKirim.classList.toggle("d-none");
+      btnLoading.classList.toggle("d-none");
+      Alert.classList.toggle("d-none");
+      form.reset();
+      console.log("Success!", response);
+    })
+    .catch((error) => console.error("Error!", error.message));
+});
