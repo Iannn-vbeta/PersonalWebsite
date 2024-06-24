eaa016dbdab4e2d5944f55e6416d5c08e620a6da
Iannnn <232410103085@mail.unej.ac.id>
2024-06-24 14:19:04 +0700

    new

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-const scriptURL = "https://script.google.com/macros/s/AKfycbz5yiohaRJrgCVGMcOrlbLy4h57v7U6AQsiGFNFCHU5OPytWRDsIHGzQCTmVrCr_cQ/exec";
-const form = document.forms["Kotak Masuk"];
+// const scriptURL = "https://script.google.com/macros/s/AKfycbz5yiohaRJrgCVGMcOrlbLy4h57v7U6AQsiGFNFCHU5OPytWRDsIHGzQCTmVrCr_cQ/exec";
+// const form = document.forms["Kotak Masuk"];
 
-form.addEventListener("submit", (e) => {
-  e.preventDefault();
-  fetch(scriptURL, { method: "POST", body: new FormData(form) })
-    .then((response) => console.log("Success!", response))
-    .catch((error) => console.error("Error!", error.message));
-});
+// form.addEventListener("submit", (e) => {
+//   e.preventDefault();
+//   fetch(scriptURL, { method: "POST", body: new FormData(form) })
+//     .then((response) => console.log("Success!", response))
+//     .catch((error) => console.error("Error!", error.message));
+// });
