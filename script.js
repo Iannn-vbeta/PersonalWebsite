d8d83c48dd83a5b6e9a4ce442d3f7afe4c10e66b
Iannnn <232410103085@mail.unej.ac.id>
2024-06-24 14:12:08 +0700

    Add Spreadseet

--- script.js
+++ script.js
@@ -1,18 +1,9 @@
-// // event pada saat link navigation di klik
-// $(".nav-item").on("click", function (e) {
-//   // ambil isi href
-//   var tujuan = $(this).attr("href");
-//   // tangkap elemen yang bersangkutan
-//   var elemenTujuan = $(tujuan);
+const scriptURL = "https://script.google.com/macros/s/AKfycbz5yiohaRJrgCVGMcOrlbLy4h57v7U6AQsiGFNFCHU5OPytWRDsIHGzQCTmVrCr_cQ/exec";
+const form = document.forms["Kotak Masuk"];
 
-//   // pindahkan scroll
-//   $("body").animate(
-//     {
-//       scrollTop: elemenTujuan.offset().top - 50,
-//     },
-//     1250,
-//     "easeInOutExpo"
-//   );
-
-//   e.preventDefault();
-// });
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  fetch(scriptURL, { method: "POST", body: new FormData(form) })
+    .then((response) => console.log("Success!", response))
+    .catch((error) => console.error("Error!", error.message));
+});
